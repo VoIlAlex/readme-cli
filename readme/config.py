d87c8c5dee82c2456034f0983c9dbc682323fdc2
VoIlAlex <ilya.vouk@gmail.com>
2020-04-15 08:32:53 +0300

    Add config.py with urls to the readme templates

---
+++ readme/config.py
@@ -0,0 +1,3 @@
+README_TEMPLATE_MEDIUM_URL = 'https://gist.githubusercontent.com/VoIlAlex/5ceb4cb4eff13faa29071610c21153ca/raw/14047fc9f9808e52a3f47aadf8e9b51c2d2e1791/README-template-medium.md'
+README_TEMPLATE_SMALL_URL = 'https://gist.githubusercontent.com/VoIlAlex/db0d4f60be9b5c5a7ef444f63effc11d/raw/be7c596a0ba3f0a037824de09629f8cf95b1bbf4/README-template-small.md'
+README_TEMPLATE_LARGE_URL = 'https://gist.githubusercontent.com/VoIlAlex/c5656213f978b570b7c571f5f1ef09f5/raw/b8ebb86188eda14a0407bf63931cc3dadb620710/README-template-large.md'
